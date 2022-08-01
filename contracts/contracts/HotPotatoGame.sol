f9e0568b50a32da043a0b1d160d8376e419a68ff
Maximiliano Gutierrez <maxigutierrez23@gmail.com>
2022-08-01 20:35:25 -0300

    All games are created through HotPotatoGame contract

--- contracts/contracts/HotPotatoGame.sol
+++ contracts/contracts/HotPotatoGame.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "./HotPotato.sol";
 
 contract HotPotatoGame is Ownable {
@@ -14,44 +13,70 @@ contract HotPotatoGame is Ownable {
 
     struct Player {
         uint256 joinedAt;
-        bool hasHotPotato;
     }
 
-    // Mapping addresses to hot potatoes
-    mapping(address => Player) public _players;
-    address[] public _playerAddrs;
+    struct Game {
+        address owner;
+        uint8 hotPotatoCount;
+        uint256 expirationTime;
+        uint256 startedAt;
+        uint256 endedAt;
+        // Mapping addresses to hot potatoes
+        mapping(address => Player) players;
+        address[] playerAddrs;
+    }
 
-    bool public _started = false;
-    bool public _ended = false;
+    // Mapping gameIds to games
+    mapping(uint256 => Game) private _games;
 
-    uint8 public _hotPotatoCount;
+    // Used for generating game ids
+    uint256 private _gameCount = 0;
 
-    uint256 public _expirationTime;
+    event GameStarted(uint256 indexed gameId);
+    event GameEnded(uint256 indexed gameId);
+    event PlayerJoined(uint256 indexed gameId, address player);
 
-    event GameStarted();
-    event GameEnded();
-    event PlayerJoined(address);
-
-    modifier gameStarted(bool started) {
+    modifier gameStarted(uint256 gameId, bool started) {
         require(
-            _started == started,
-            _started ? "Game hasn't started yet" : "Game already started"
+            (_games[gameId].startedAt > 0) == started,
+            started ? "Game hasn't started yet" : "Game already started"
         );
         _;
     }
 
-    modifier gameEnded(bool ended) {
+    modifier gameEnded(uint256 gameId, bool ended) {
         require(
-            _ended == ended,
-            _ended ? "Game hasn't ended yet" : "Game has ended"
+            (_games[gameId].endedAt == 0) == ended,
+            ended ? "Game hasn't ended yet" : "Game has ended"
         );
         _;
     }
 
-    constructor(address hotPotatoContract, uint256 expirationTime)
-        public
-        Ownable()
-    {
+    modifier onlyOwner(uint256 gameId) {
+        require(
+            _games[gameId].owner == msg.sender,
+            "Sender should be game owner"
+        );
+        _;
+    }
+
+    constructor(address hotPotatoContract) public {
+        _hotPotatoContract = IERC721(hotPotatoContract);
+    }
+
+    function createGame() public {
+        uint256 gameId = uint256(keccak256(abi.encodePacked(_gameCount++)));
+
+        _games[gameId] = new Game({owner: msg.sender, playerAddrs: []});
+
+        _addPlayer(gameId, msg.sender);
+    }
+
+    function startGame(
+        uint256 gameId,
+        uint8 hotPotatoCount,
+        uint256 expirationTime
+    ) onlyOwner(gameId) gameStarted(gameId, false) {
         require(
             now < expirationTime,
             "Expiration time should be in the future"
@@ -60,43 +85,52 @@ contract HotPotatoGame is Ownable {
             now + MAX_FUTURE_EXPIRATION_TIME >= expirationTime,
             "Expiration time exceeds limit"
         );
+        require(_games[gameId].playerAddrs.length > 1, "Not enough players");
+        require(
+            hotPotatoCount < _games[gameId].playerAddrs.length,
+            "To many hot potatoes"
+        );
 
-        _owner = tx.origin;
-
-        _hotPotatoContract = IERC721(hotPotatoContract);
-        _expirationTime = expirationTime;
-
-        _addPlayer(_owner);
-    }
-
-    function startGame(uint8 hotPotatoCount) onlyOwner gameStarted(false) {
-        require(_playerCount > 1, "Not enough players");
-        require(hotPotatoCount < _playerCount, "To many hot potatoes");
+        _games[gameId].hotPotatoCount = hotPotatoCount;
+        _games[gameId].expirationTime = expirationTime;
 
         // TODO: Create hot potatoes
 
-        _started = true;
+        _games[gameId].expirationTime.started = true;
 
-        emit GameStarted();
+        emit GameStarted(gameId);
     }
 
-    function joinGame() public gameStarted(false) {
-        require(_players.length < MAX_PLAYERS, "Already at max players");
-        require(_players[msg.sender].joinedAt == 0, "Player already in game");
+    function joinGame(uint256 gameId) public gameStarted(gameId, false) {
+        require(
+            _games[gameId].playerAddrs.length < MAX_PLAYERS,
+            "Already at max players"
+        );
+        require(
+            _games[gameId].players[msg.sender].joinedAt == 0,
+            "Player already in game"
+        );
 
-        _addPlayer(msg.sender);
+        _addPlayer(gameId, msg.sender);
 
-        emit PlayerJoined(msg.sender);
+        emit PlayerJoined(gameId, msg.sender);
     }
 
-    function _addPlayer(address addr) internal {
-        _players[addr] = new Player({joinedAt: now, hasHotPotato: false});
-        _playerAddrs.push(addr);
+    function _addPlayer(uint256 gameId, address player) internal {
+        _games[gameId].players[player] = new Player({joinedAt: now});
+        _games[gameId].playerAddrs.push(player);
+
+        emit PlayerJoined(gameId, player);
     }
 
-    function getWinners() view gameEnded(true) returns (uint256[]) {
+    function getWinners(uint256 gameId)
+        view
+        gameEnded(gameId, true)
+        returns (uint256[] winners)
+    {
         uint256[] winners;
         for (uint8 i = 0; i < _playerAddrs.length; i++) {
+            // TODO: Check HotPotato contract to see if each player owns hot potato
             if (_players[_playerAddrs[i]]) {
                 winners.push(_playerAddrs[i]);
             }
