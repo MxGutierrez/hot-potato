9911f95b349384cad5efd4f87fd53a438eb508d7
Maximiliano Gutierrez <maxigutierrez23@gmail.com>
2022-08-15 22:51:38 -0300

    Decrease distance to address label

--- client/src/components/Table/Address.jsx
+++ client/src/components/Table/Address.jsx
@@ -1,19 +1,15 @@
-import { Graphics, Container, Text } from "@inlet/react-pixi";
+import { Text } from "@inlet/react-pixi";
 import { TextStyle } from "pixi.js";
-import { useCallback, useRef } from "react";
 
 function Address({ address, x, y }) {
-  const test = useRef(null);
-
   return (
     <Text
-      ref={test}
       text={`${address.substring(0, 5)}…${address.substring(
         address.length - 4
       )}`}
       anchor={0.5}
       x={x}
-      y={y - 70}
+      y={y - 65}
       scale={0.7}
       style={
         new TextStyle({
